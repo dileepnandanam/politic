23d4ec69e1fd2985eb5eb2fe78c3deae92fb74fc
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-03-04 14:58:59 +0530

    survey responses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_090950) do
+ActiveRecord::Schema.define(version: 2020_03_04_091223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_090950) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "survey_response_id"
   end
 
   create_table "chats", force: :cascade do |t|
