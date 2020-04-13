7e4ea4a064cec7a75c5608e8f1ccb4baa60a52fb
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-04-13 09:58:28 +0530

    hide controlls on preview

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,7 +7,7 @@ class Group < ApplicationRecord
   #validates :description, presence: true
   has_many :group_responses
   has_many :users, through: :group_responses
-  belongs_to :post, optional: true
+  has_many :welcome_posts, foreign_key: :project_id, class_name: 'Post'
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
