11e00fd5229d8c90c8ccc3a9b8e9f045ce61c1ce
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-04-17 22:01:15 +0530

    multiple notification error fixed

--- app/assets/javascripts/channels/survey_notifications_channel.js
+++ app/assets/javascripts/channels/survey_notifications_channel.js
@@ -1,9 +1,14 @@
+survey_notification_subscribed = null
 $(document).on('turbolinks:load', function() {
   audio = new Audio($('.response-notif').attr('src'));
-  App.cable.subscriptions.create("ApplicationCable::SurveyNotificationsChannel", {
-    received(data) {
-      audio.play()
-      $('.model-response-viewer').append(data.message)
-    }
-  })
+  
+  if(survey_notification_subscribed == null) {
+    App.cable.subscriptions.create("ApplicationCable::SurveyNotificationsChannel", {
+      received(data) {
+        audio.play()
+        $('.model-response-viewer').append(data.message)
+      }
+    })
+    survey_notification_subscribed = 1
+  }
 })
