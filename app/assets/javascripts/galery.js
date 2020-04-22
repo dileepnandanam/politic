76b389cdab1f181931f3f739c4978a559effea1e
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-04-22 17:26:39 +0530

    making add picture user friendly

--- app/assets/javascripts/galery.js
+++ app/assets/javascripts/galery.js
@@ -11,19 +11,19 @@ $(document).on('turbolinks:load', function() {
   bind_picture_add()
   $(document).on('ajax:success', '.new-galery', function(e) {
     $(this).closest('.post').find('.galery-form-container').html(e.detail[2].responseText)
-    $('.cancel').click(function(e) {
-      e.preventDefault()
-      $(this).closest('form').remove()
-    })
   })
 
-  $(document).on('ajax:success', '.galery-form', function(e) {
-    $(document).off('ajax:success', '.galery-form')
-    %(this).remove()
-    $('.galeries-container').append(e.detail[2].responseText)
-    prepare_asinc_reload()
-    bind_picture_add()   
-  })
+  bind_galery_create = function() {
+    $(document).on('ajax:success', '.galery-form', function(e) {
+      $(document).off('ajax:success', '.galery-form')
+      %(this).remove()
+      $('.galeries-container').append(e.detail[2].responseText)
+      prepare_asinc_reload()
+      bind_picture_add()
+      bind_galery_create()
+    })
+  }
+  bind_galery_create()
 
   $(document).on('ajax:success', '.edit-galery-form', function(e) {
     $(this).closest('.galery').replaceWith(e.detail[2].responseText)
@@ -38,12 +38,16 @@ $(document).on('turbolinks:load', function() {
     $(this).closest('.galery').remove()
   })
 
-  $(document).on('ajax:success', '.add-picture', function(e) {
-    $(document).off('ajax:success', '.add-picture')
-    $(this).closest('.galery').find('.pictures').append(e.detail[2].responseText)
-    %(this).remove()
-    prepare_asinc_reload()
-  })
+  bind_picture_add = function() {
+    $(document).on('ajax:success', '.add-picture', function(e) {
+      $(document).off('ajax:success', '.add-picture')
+      $(this).closest('.galery').find('.pictures').append(e.detail[2].responseText)
+      %(this).remove()
+      prepare_asinc_reload()
+      bind_picture_add()
+    })
+  }
+  bind_picture_add()
 
   $(document).on('ajax:success', '.remove-picture', function(e) {
     $(this).closest('.picture').remove()
@@ -55,8 +59,9 @@ $(document).on('turbolinks:load', function() {
   })
 
   $(document).on('ajax:success', '.add-galery-picture', function(e) {
-    $('.picture-form-container').html(e.detail[2].responseText)
+    $(this).siblings('.picture-form-container').html(e.detail[2].responseText)
   })
+
   $(document).on('ajax:success', '.edit-galery-picture', function(e) {
     $(this).siblings('.edit-form-container').html(e.detail[2].responseText)
   })
--- app/models/galery.rb
+++ app/models/galery.rb
@@ -2,4 +2,5 @@ class Galery < ApplicationRecord
   has_many :pictures
   belongs_to :user
   belongs_to :post
+  default_scope -> {order('id ASC')}
 end
