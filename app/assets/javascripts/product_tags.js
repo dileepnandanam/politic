c7afe85e562400d6d6e8d99ee0347f1a01a017ac
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-04-26 00:34:27 +0530

    tag piramid

--- app/assets/javascripts/product_tags.js
+++ app/assets/javascripts/product_tags.js
@@ -18,6 +18,31 @@ $(document).on('turbolinks:load', function() {
       }
     })
   }
-  $('.search-tags').keyup($.debounce(1250, search))
+
+
+
+  $(document).on('keypress', '.add-tag', function(e){
+    if(e.which == 13) {
+      tag = $(this).val()
+      that = $(this)
+      parent_id = $(this).data().parantId
+      $.ajax({
+        url: '/product_tags',
+        method: 'POST',
+        data: {
+          product_tag: {
+            name: tag,
+            parent_id: parent_id
+          }
+        },
+        beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+        success: function(e) {
+          $('input').val('')
+          $(that).closest('.product-tags').append(e)
+        }
+      })
+    }
+  })
+
 
 })
--- app/controllers/product_tags_controller.rb
+++ app/controllers/product_tags_controller.rb
@@ -12,4 +12,15 @@ class ProductTagsController < ApplicationController
       render partial: 'product_tags', locals: {product_tags: @product_tags}, alyout: false
     end
   end
+
+  def create
+    if current_user.admin?
+      @tag = ProductTag.create(product_tag_params)
+      render partial: 'product_tag', locals: {t: @tag} and return
+    end
+  end
+
+  def product_tag_params
+    params.require(:product_tag).permit(:parent_id, :name)
+  end
 end
