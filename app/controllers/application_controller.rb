5916381eca1b126d8e39c16382f02896d33943be
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-04-30 10:14:07 +0530

    nossl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :set_https
 
   def set_https
-    if Rails.env != "development"
+    if Rails.env != "development" && false
       unless request.url.starts_with?('https')
         redirect_to request.url.gsub('http', 'https') and return
       end
