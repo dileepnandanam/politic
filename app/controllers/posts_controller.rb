b33ce3be4877af65c62a348c0403bbabaec62ba2
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-05-09 17:58:12 +0530

    ordering

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,7 +48,7 @@ class PostsController < PostBaseController
       @posts = Post.search(params[:query], nil, orientation, [current_user.try(:lat), current_user.try(:lngt)])
       @posts = @posts.paginate(per_page: 12, page: params[:page])
     else
-      @posts = Post.where(group_id: nil).all
+      @posts = Post.order('id ASC').where(group_id: nil).all
       @posts = @posts.paginate(per_page: 12, page: params[:page])
     end
 
