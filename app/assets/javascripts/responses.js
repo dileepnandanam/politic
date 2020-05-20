24a750ad6535b20058ca9260a9b7fc39eec9a187
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-05-20 18:39:54 +0530

    smothening submit survey

--- app/assets/javascripts/responses.js
+++ app/assets/javascripts/responses.js
@@ -1,6 +1,10 @@
 $(document).on('turbolinks:load', function() {
   $(document).on('ajax:success', '.quick-poll-form, .survey_response_form', function(e) {
+    var container = $(this).closest('.question')
+    var height = $(container).css('height')
     $(this).replaceWith(e.detail[2].responseText)
+    $(container).css('height', height)
+    $(container).animate({height: '300px'}, 1000)
   })
 
   $(document).on('ajax:error', '.quick-poll-form, .survey_response_form', function(e) {
