21c5059398bab00ca31a65e1d17a5bace2598afb
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-06-07 17:09:31 +0530

    center align pictures

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,7 +54,7 @@ class PostsController < PostBaseController
         render 'posts', layout: false and return
       end
     else
-      @posts = Post.where(group_id: nil).order('id asc')
+      @posts = Post.where(group_id: nil, published: true).order('id asc')
       @posts = @posts.paginate(per_page: 12, page: params[:page])
       @next_path = posts_path(page: (params[:page].present? ? params[:page].to_i + 1 : 2), query: params[:query])
       
